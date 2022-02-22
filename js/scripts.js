62423c8679c7f045e7fdee7a611d56ae9b16e9bb
lmjpawaon <lmjpawaon@addu.edu.ph>
2022-02-22 20:26:54 +0800

    update scripts - daghan pa kulang mao ra

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,39 @@
 //loader function
 $(window).on('load',function(){
-    $(".loader").fadeOut("slow");
-    $(".content").fadeIn(1000);
+    $(".loader").fadeOut(1500);
 });
 
-
+$('.carousel').slick({
+    infinite: true,
+    centerMode:true,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 3,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
