6ba029dd205cebe05af3fa8852949135b1e36a7f
lmjpawaon <lmjpawaon@addu.edu.ph>
2022-02-23 12:39:52 +0800

    update scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,19 @@
-//loader function
-$(window).on('load',function(){
-    $(".loader").fadeOut(1500);
+$('.hero-carousel').slick({
+    autoplay: true,
+    autoplayspeed: 1500,
+    centerMode:true,
+    arrows:false,
 });
 
 $('.carousel').slick({
     infinite: true,
+    arrows: true,
+    prevArrow:'<button class="prevBtn"><span class="iconify" data-icon="ooui:previous-ltr" style="color: #f5f5f1;"></span></button>',
+    nextArrow:'<button class="nxtBtn"><span class="iconify" data-icon="ooui:previous-rtl" style="color: #f5f5f1;"></span></button>',
     centerMode:true,
     speed: 300,
     slidesToShow: 4,
-    slidesToScroll: 3,
+    slidesToScroll: 4,
     responsive: [
       {
         breakpoint: 1024,
@@ -32,8 +37,33 @@ $('.carousel').slick({
           slidesToScroll: 1
         }
       }
-      // You can unslick at a given breakpoint now by adding:
-      // settings: "unslick"
-      // instead of a settings object
     ]
   });
+
+$(document).ready(function() {
+
+    //loader function
+  $(window).on('load',function(){
+    $(".loader").fadeOut(1500);
+  });
+    
+  $(window).scroll( function(){
+  
+      $('.fade-in').each( function(i){
+          
+          var bottom_of_object = $(this).position().top + $(this).outerHeight();
+          var bottom_of_window = $(window).scrollTop() + $(window).height();
+          
+          if( bottom_of_window > bottom_of_object ){
+              
+              $(this).animate({'opacity':'1'},500);
+                  
+          }
+          
+      }); 
+  
+  });
+  
+});
+
+
